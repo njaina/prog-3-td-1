aa750443e6162b3aa59f4ea10cf8bcade37df33f
njaina <hei.njaina.2@gmail.com>
2022-12-13 08:19:52 +0300

    feat: add more featuring in repository

--- src/main/java/app/prog/repository/AuthorRepository.java
+++ src/main/java/app/prog/repository/AuthorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Integer> {
+    AuthorEntity findByName(String author);
 }
--- src/main/java/app/prog/repository/CategoryRepository.java
+++ src/main/java/app/prog/repository/CategoryRepository.java
@@ -1,2 +1,9 @@
-package app.prog.repository;public interface CategoryRepository {
+package app.prog.repository;
+
+import app.prog.model.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity,Integer> {
 }
